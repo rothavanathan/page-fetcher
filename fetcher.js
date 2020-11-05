731957901ba476140273a954098d66ad0508b491
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-11-05 01:26:07 +0000

    quick refactor of writefile function

--- fetcher.js
+++ fetcher.js
@@ -7,30 +7,39 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-//save argument to variable
+//save commandline arguments to variables
 const url = process.argv[2];
 const filename = process.argv[3];
+
+const writeFile = (filename, body) => {
+  fs.writeFile(filename, body, (err) => {
+    //handle error on write
+    if (err) throw err;
+    console.log(`Downloaded and saved ${body.length} bytes to ${filename}`);
+  });
+};
+
 
 //check if file already exists
 if (fs.existsSync('/etc/passwd')) {
   rl.question(`File already exists. Would you like to write over? Y / N `, (answer) => {
+    //yes means try overwrite
     if (answer === "Y" || answer === "y") {
       request(url, (error, response, body) => {
+        //handle error
         if (error) {
           console.log('There was an error!', error);
           console.log('statusCode:', response && response.statusCode);
+        //write that file
         } else {
-          console.log('statusCode:', response && response.statusCode);
-          console.log('body:', body);
-          fs.writeFile(filename, body, (err) => {
-            if (err) throw err;
-            console.log(`Downloaded and saved ${body.length} bytes to ${filename}`);
-          });
+          writeFile(filename, body);
         }
       });
+    //no overwrite
     } else {
       console.log(`ok`);
     }
+    //close it up
     rl.close();
   });
 }
